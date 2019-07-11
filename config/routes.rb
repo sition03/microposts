abd3cb03f75184d927c55a837b14758a5d3f98d7
takuto.futakawame <takuto3213@gmail.com>
2019-07-11 04:42:57 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,19 @@ Rails.application.routes.draw do
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "login", to: "sessions#destroy"
+  delete "logout", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :new, :create]
+  
+  resources :users, only: [:index, :show, :new, :create] do
+    member do 
+      get :followings
+      get :followers
+    end
+
+  end
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
 end
